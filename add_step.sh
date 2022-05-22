82961655ab12f214c2ffb8feba588a68b2b6e728
Elior Malul <elior@twigo.tv>
2022-05-22 13:31:50 +0300

    1) Fixed memory allocation for steps. 2) More flexible add_steps. 3) Added workers to the cluster's fleet.

--- add_step.sh
+++ add_step.sh
@@ -4,6 +4,12 @@ if [ -z $CLUSTER_ID ]; then
     echo "Please set CLUSTER_ID as env. variable"
     exit
 fi
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./add_step.sh <steps-file-path>"
+    exit
+fi
+
 aws emr add-steps \
     --cluster-id $CLUSTER_ID \
-    --steps file://steps.json
+    --steps file://$1
