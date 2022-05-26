b22c717ab0d27fc08ac648cfcfd27e0d720563c6
Elior Malul <elior@twigo.tv>
2022-05-26 14:33:52 +0300

    tweaks

--- create-cluster.sh
+++ create-cluster.sh
@@ -4,10 +4,9 @@ aws emr create-cluster \
     --name "Spark cluster for data digestion" \
     --release-label "emr-6.5.0" \
     --applications Name=Spark \
-    --steps file://steps.json \
     --use-default-roles  \
     --ec2-attributes SubnetId=subnet-0ad138931468c444f,KeyName=lzg-dev-emr-master \
     --instance-fleets file://fleets.json \
     --log-uri s3://lzg-dev-logs/ \
-    --auto-termination-policy IdleTimeout=60 \
+    --auto-termination-policy IdleTimeout=1000 \
  | tee .cluster_id
