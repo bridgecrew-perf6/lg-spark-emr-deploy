13940ae6df72a33e5ee055729a01d3932778cd7c
Elior Malul <elior@twigo.tv>
2022-05-30 17:56:50 +0300

    Bugfix: add_step

--- add_step.sh
+++ add_step.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-if [ -z $CLUSTER_ID ]; then
+if [[ -z $CLUSTER_ID && -f .cluster_id ]]; then
     echo "Reading cluster ID from .cluster_id"
     CLUSTER_ID=$(jq '.ClusterId' .cluster_id | tr -d '"')
 fi
