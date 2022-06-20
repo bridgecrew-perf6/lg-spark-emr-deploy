595b54622cad9ae5eca513b7eedc6be1fad542de
Elior Malul <elior@twigo.tv>
2022-06-20 18:07:19 +0300

    Fleets adjusted to scale

--- create-cluster.sh
+++ create-cluster.sh
@@ -8,5 +8,5 @@ aws emr create-cluster \
     --ec2-attributes SubnetId=subnet-0ad138931468c444f,KeyName=lzg-dev-emr-master \
     --instance-fleets file://fleets.json \
     --log-uri s3://lzg-dev-logs/ \
-    --auto-termination-policy IdleTimeout=1000 \
+    --auto-termination-policy IdleTimeout=3000 \
  | tee .cluster_id
